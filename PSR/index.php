7f031a62e57d5b5211a496d3e9bcabfe4301f1ff
Aislan Santos <aislan_85@yahoo.com.br>
2020-07-30 22:21:19 -0300

    inserindo repositório/ gerenciamento de dependencia

--- PSR/index.php
+++ PSR/index.php
@@ -1,5 +1,7 @@
 <?php
 require 'autoload.php';
+// abaixo mostra as duas formas de trabalhar com o auload
+
 
 //forma de usar informando o caminho antes prara no instanciamento somente usar o nome da classe
 use \fotos\Upload;
