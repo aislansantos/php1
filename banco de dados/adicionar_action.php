3e37233e2135c106b8b60defc8ac59a9d5e5cd71
Aislan Santos <aislan_85@yahoo.com.br>
2020-07-31 22:38:53 -0300

    aula insert banco de dados

--- banco de dados/adicionar_action.php
+++ banco de dados/adicionar_action.php
@@ -6,10 +6,19 @@ $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
 if ($name && $email) {
 
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql->bindValue(':email', $email);
+    $sql->execute();
+
+    
+
     $sql = $pdo->prepare("INSERT INTO usuarios (nome,email) VALUES (:name, :email)");
     $sql->bindValue(':name', $name);
-    $sql->bindParam(':email', $email);
+    $sql->bindValue(':email', $email);
+    $sql->execute();
 
+    header("location: index.php");
+    exit;
 } else {
     header("location: adicionar.php");
 }
