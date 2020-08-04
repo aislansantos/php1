c2bccb3201e525105f5066c7167f4337238deb0c
Aislan Santos <aislan_85@yahoo.com.br>
2020-08-04 16:33:08 -0300

    aula banco de dados, criação da classe usuario e dentro dela a interface para a DAO

--- banco de dados/models/Usuario.php
+++ banco de dados/models/Usuario.php
@@ -1,5 +1,53 @@
 <?php
 
-class Usuario{
-    
+class Usuario
+{
+
+    private $id;
+    private $nome;
+    private $email;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($i)
+    {
+        //trim tira eventuais espaços no caso de id receber numero e letras, é uma boa pratica
+        $this->id = trim($i);
+    }
+
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+    public function setNome($n)
+    {
+        /* ucwords coloca cada letra de cada palavra nesse caso nome, como maiuscula, a função ucfirst coloca a 
+        primeira palavra somente como maiuscula como inicio de frase */
+        $this->nome = ucwords(trim($n));
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    public function setEmail($e)
+    {
+        // strtolowerconverte todo o texto para minuscula
+        $this->email = strtolower(trim($e));
+    }
+}
+
+/*Vamos criar uma interface para que as classes que todas as classes que forem fazer uso da classe usuario,
+vai ter de seguir a implementação dessa interface */
+interface usuarioDAO{
+    public function add(Usuario $u);
+    public function findAll();
+    public function findById($id);
+    public function update(Usuario $u);
+    public function delete($id);
 }
